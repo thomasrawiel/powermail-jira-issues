76b9ade4ec23af02271ec7a3ec7201c47557ded9
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2024-07-24 15:45:30 +0200

    init

---
+++ ext_emconf.php
@@ -0,0 +1,20 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Powermail JIRA Issues',
+    'description' => 'Post powermail form submissions as jira issues',
+    'category' => 'misc',
+    'author' => 'Thomas Rawiel',
+    'author_email' => 'thomas.rawiel@gmail.com',
+    'state' => 'beta',
+    'clearCacheOnLoad' => 0,
+    'version' => '0.1.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.4.0-13.9.99',
+            'powermail' => '10.0.0-12.99.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
