36f02e7a8ec524bf36eb7e9db329e4ec43264b5f
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2024-08-30 15:36:28 +0200

    remove moved files

--- Configuration/TCA/Overrides/sys_template.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-defined('TYPO3') || die('Access denied.');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('powermail_jira_issues', 'Configuration/TypoScript', 'Add Powermail Jira Issues Finisher');
